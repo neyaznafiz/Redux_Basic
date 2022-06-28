6f663a144d944f5ca4e8c3e76340ae083442e257
niiyazobug <niiyaz5555@gmail.com>
2022-06-28 20:51:05 +0600

    payload use method and add user with count method

--- recap.js
+++ recap.js
@@ -6,53 +6,127 @@
 const { createStore } = require("redux")
 
 // variables
-const INCREMENT = 'INCREMENT'
-const DECREMENT = 'DECREMENT'
-const RESET = 'RESET'
+// const INCREMENT = 'INCREMENT'
+// const INCREMENT_BY_VALUE = 'INCREMENT_BY_VALUE'
+// const DECREMENT = 'DECREMENT'
+// const RESET = 'RESET'
 
-const initialState = {
-    count: 0
-}
+// const initialState = {
+//     count: 0
+// }
 
 
-const incrementCounterAction = () => {
-    return {
-        type: INCREMENT
-    }
-}
+// const addUser = () => {
+//     return {
+//         type: ADD_USER
+//     }
+// }
 
-const decrementCounterAction = () => {
-    return {
-        type: DECREMENT
-    }
-}
+// const incrementCounterAction = () => {
+//     return {
+//         type: INCREMENT
+//     }
+// }
 
-const resetCounterAction = () => {
-    return {
-        type: RESET
-    }
-}
+// const decrementCounterAction = () => {
+//     return {
+//         type: DECREMENT
+//     }
+// }
+
+// const resetCounterAction = () => {
+//     return {
+//         type: RESET
+//     }
+// }
+
+// // using payload
+// const incrementCounterByValue = (value) => {
+//     return {
+//         type: INCREMENT_BY_VALUE,
+//         payload: value
+//     }
+// }
 
 
 // creating reducer
-const counterReducer = (state = initialState, action) => {
+// const counterReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case INCREMENT:
+//             return {
+//                 ...state,
+//                 count: state.count + 1
+//             }
+
+//         case DECREMENT:
+//             return {
+//                 ...state,
+//                 count: state.count - 1
+//             }
+
+//         case RESET:
+//             return {
+//                 ...state,
+//                 count: 0
+//             }
+
+
+//         // reducer for payload method
+//         case INCREMENT_BY_VALUE:
+//             return {
+//                 ...state,
+//                 count: state.count + action.payload
+//             }
+
+//         default:
+//             state
+//     }
+// }
+
+
+// store
+// const store = createStore(counterReducer)
+
+// store.subscribe(() => {
+//     console.log(store.getState())
+// })
+
+// store.dispatch(incrementCounterAction())
+// store.dispatch(incrementCounterAction())
+// store.dispatch(incrementCounterAction())
+// store.dispatch(decrementCounterAction())
+// store.dispatch(resetCounterAction())
+// store.dispatch(incrementCounterAction())
+
+// for by value
+// store.dispatch(incrementCounterByValue(5))
+// store.dispatch(incrementCounterByValue(10))
+
+
+// ----------------------------------------------------------------------------
+
+
+const ADD_USER = 'ADD_USER'
+
+const initialState = {
+    users: ['Neyaz'],
+    count: 1
+}
+
+
+const addUser = (user) => {
+    return {
+        type: ADD_USER,
+        payload: user
+    }
+}
+
+const userReducer = (state = initialState, action) => {
     switch (action.type) {
-        case INCREMENT:
+        case ADD_USER:
             return {
-                ...state,
+                users: [...state.users, action.payload],
                 count: state.count + 1
-            }
-
-        case DECREMENT:
-            return {
-                ...state,
-                count: state.count - 1
-            }
-
-        case RESET:
-            return {
-                ...state,
-                count: 0
             }
 
         default:
@@ -60,17 +134,11 @@ const counterReducer = (state = initialState, action) => {
     }
 }
 
-
-// store
-const store = createStore(counterReducer)
+const store = createStore(userReducer)
 
 store.subscribe(() => {
-console.log(store.getState())
+    console.log(store.getState())
 })
 
-store.dispatch(incrementCounterAction())
-store.dispatch(incrementCounterAction())
-store.dispatch(incrementCounterAction())
-store.dispatch(decrementCounterAction())
-store.dispatch(resetCounterAction())
-store.dispatch(incrementCounterAction())
+store.dispatch(addUser('Mobalik'))
+store.dispatch(addUser('Nafiz'))
